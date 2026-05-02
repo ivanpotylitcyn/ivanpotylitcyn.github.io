e30b818629bd173c3c6c7bfbb6c42a5ecb41f10d
Иван Потылицын <potylitcyn@phystech.edu>
2026-05-02 23:43:42 +0300

    Задал дефолтный домен

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,5 @@ import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   output: 'static',
-  site: 'https://ivanpotylitcyn.ru',
+  site: 'https://ivanpotylitcyn.github.io',
 });
